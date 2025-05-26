fe709c6a8d07383139cece2ffe7c274b9d1d4818
brenohabib <brenohabib2013@gmail.com>
2025-05-26 11:08:28 -0300

    Adicionando lógica para registro e login de usuário com criptografia

--- src/main/java/com/example/controller/UsuarioController.java
+++ src/main/java/com/example/controller/UsuarioController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api/usuario")
 @CrossOrigin(origins = "*")
 public class UsuarioController {
 
